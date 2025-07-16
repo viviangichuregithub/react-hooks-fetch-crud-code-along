97d715d80e1cab51d57d9b1d0ae87cb81b4a80d7
Vivian Gichure <viviangichure@gmail.com>
2025-07-16 16:14:24 +0300

    completed lab

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,36 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = {
+      name,
+      category,
+      isInCart: false,
+    };
+
+    // Send new item to server
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newItem),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        onAddItem(data); // Notify parent
+        setName(""); // Reset form
+        setCategory("Produce");
+      });
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
